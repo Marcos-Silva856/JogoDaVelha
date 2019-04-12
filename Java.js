28189ecac90dfccc2c176910afce8caf0085ce89
Marcos-Silva856 <49396752+Marcos-Silva856@users.noreply.github.com>
2019-04-12 10:19:37 -0300

    Add files via upload

--- Java.js
+++ Java.js
@@ -7,6 +7,9 @@ var contx = 0;
 var conto = 0;
 var fim = false;
 var velha;
+var colorX = false;
+var colorY = false;
+
 for (var i = 0; i < 3; i++) {
     jogo[i] = [];
 }
@@ -17,10 +20,16 @@ for (var i = 0; i < 3; i++) {
 }
 function ClickX() {
     if (bool == false) {
-        document.getElementById('Xizinho').style.backgroundColor = '#E0E0E0';
+        document.getElementById('Xizinho').style.backgroundColor = '#212121';
+        document.getElementById('EscX').src = 'XISFoco.png';
         document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+        document.getElementById('EscO').src = 'Bola.png';
         vez = "X";
+        colorX = true;
+        colorY = false;
+
         return vez;
+
     }
 
 
@@ -28,8 +37,12 @@ function ClickX() {
 function ClickY() {
     if (bool == false) {
         vez = "O";
-        document.getElementById('Bolinha').style.backgroundColor = '#E0E0E0';
+        document.getElementById('Bolinha').style.backgroundColor = '#212121';
+        document.getElementById('EscO').src = 'BolaFoco.png';
         document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+        document.getElementById('EscX').src = 'XIS.png';
+        colorY = true;
+        colorX = false;
         return vez;
     }
 }
@@ -46,6 +59,7 @@ function vencedor() {
                 document.getElementById('PX').innerHTML = contx;
                 fim = true;
                 document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+                document.getElementById('EscO').src = 'Bola.png';
                 document.getElementById('Xizinho').style.backgroundColor = '#E0E0E0';
                 document.getElementById('res').style.display = 'block';
                 document.getElementById('res').style.backgroundImage = "url('XVence.png')";
@@ -63,6 +77,7 @@ function vencedor() {
                 fim = true;
                 document.getElementById('Bolinha').style.backgroundColor = '#E0E0E0';
                 document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+                document.getElementById('EscX').src = 'XIS.png';
                 document.getElementById('res').style.display = 'block';
                 document.getElementById('res').style.backgroundImage = "url('OVence.png')";
                 document.getElementById('res').style.backgroundRepeat = "no-repeat";
@@ -80,6 +95,7 @@ function vencedor() {
                 document.getElementById('PX').innerHTML = contx;
                 fim = true;
                 document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+                document.getElementById('EscO').src = 'Bola.png';
                 document.getElementById('Xizinho').style.backgroundColor = '#E0E0E0';
                 document.getElementById('res').style.display = 'block';
                 document.getElementById('res').style.backgroundImage = "url('XVence.png')";
@@ -98,6 +114,7 @@ function vencedor() {
                 fim = true;
                 document.getElementById('Bolinha').style.backgroundColor = '#E0E0E0';
                 document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+                document.getElementById('EscX').src = 'XIS.png';
                 document.getElementById('res').style.display = 'block';
                 document.getElementById('res').style.backgroundImage = "url('OVence.png')";
                 document.getElementById('res').style.backgroundRepeat = "no-repeat";
@@ -118,6 +135,7 @@ function vencedor() {
             document.getElementById('PX').innerHTML = contx;
             fim = true;
             document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+            document.getElementById('EscO').src = 'Bola.png';
             document.getElementById('Xizinho').style.backgroundColor = '#E0E0E0';
             document.getElementById('res').style.display = 'block';
             document.getElementById('res').style.backgroundImage = "url('XVence.png')";
@@ -135,6 +153,7 @@ function vencedor() {
             fim = true;
             document.getElementById('Bolinha').style.backgroundColor = '#E0E0E0';
             document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+            document.getElementById('EscX').src = 'XIS.png';
             document.getElementById('res').style.display = 'block';
             document.getElementById('res').style.backgroundImage = "url('OVence.png')";
             document.getElementById('res').style.backgroundRepeat = "no-repeat";
@@ -153,6 +172,7 @@ function vencedor() {
             document.getElementById('PX').innerHTML = contx;
             fim = true;
             document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+            document.getElementById('EscO').src = 'Bola.png';
             document.getElementById('Xizinho').style.backgroundColor = '#E0E0E0';
             document.getElementById('res').style.display = 'block';
             document.getElementById('res').style.backgroundImage = "url('XVence.png')";
@@ -170,6 +190,7 @@ function vencedor() {
             fim = true;
             document.getElementById('Bolinha').style.backgroundColor = '#E0E0E0';
             document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+            document.getElementById('EscX').src = 'XIS.png';
             document.getElementById('res').style.display = 'block';
             document.getElementById('res').style.backgroundImage = "url('OVence.png')";
             document.getElementById('res').style.backgroundRepeat = "no-repeat";
@@ -191,7 +212,9 @@ function vencedor() {
     }
     if (velha == 9) {
         document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+        document.getElementById('EscO').src = 'Bola.png';
         document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+        document.getElementById('EscX').src = 'XIS.png';
         document.getElementById('res').style.display = 'block';
         document.getElementById('res').style.backgroundImage = "url('velhas.gif')";
         document.getElementById('res').style.backgroundRepeat = "no-repeat";
@@ -204,6 +227,8 @@ function vencedor() {
 function ClickE(n1) {
     var mat = (n1.id).toString();
     if (fim == false) {
+        colorX = true;
+        colorY = true;
         if (jogo[mat[1]][mat[2]] == "A") {
             img = document.getElementById('img' + n1.id);
             if (vez == "X") {
@@ -211,22 +236,26 @@ function ClickE(n1) {
                 jogo[mat[1]][mat[2]] = "X";
                 img.src = "XIS.png"
                 vez = 'O';
-                document.getElementById('Bolinha').style.backgroundColor = '#E0E0E0';
+                document.getElementById('Bolinha').style.backgroundColor = '#212121';
+                document.getElementById('EscO').src = 'BolaFoco.png';
                 document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+                document.getElementById('EscX').src = 'XIS.png';
             } else if (vez == "O") {
                 bool = true;
                 jogo[mat[1]][mat[2]] = "O";
                 img.src = "Bola.png"
                 vez = 'X';
                 document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
-                document.getElementById('Xizinho').style.backgroundColor = '#E0E0E0';
-            }else if(vez=="nada"){
+            document.getElementById('EscO').src = 'Bola.png';
+                document.getElementById('Xizinho').style.backgroundColor = '#212121';
+            document.getElementById('EscX').src = 'XISFoco.png';
+            } else if (vez == "nada") {
                 document.getElementById('Trat').style.display = 'block';
-        document.getElementById('Trat').style.backgroundImage = "url('Tratamento.png')";
-        document.getElementById('Trat').style.backgroundRepeat = "no-repeat";
-        document.getElementById('Trat').style.backgroundPosition = "center";
-        document.getElementById('newG').style.display = 'none';
-        document.getElementById('newT').style.display = 'block';
+                document.getElementById('Trat').style.backgroundImage = "url('Tratamento.png')";
+                document.getElementById('Trat').style.backgroundRepeat = "no-repeat";
+                document.getElementById('Trat').style.backgroundPosition = "center";
+                document.getElementById('newG').style.display = 'none';
+                document.getElementById('newT').style.display = 'block';
 
             }
         }
@@ -237,27 +266,85 @@ function NewGame() {
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
             jogo[i][j] = "A";
-            img = 'A'+i.toString() + j.toString();
+            img = 'A' + i.toString() + j.toString();
             img = document.getElementById('img' + img);
             img.src = "Branco.png";
-            document.getElementById('A'+j.toString() + i.toString()).style.backgroundColor = '#BDBDBD';
+            document.getElementById('A' + j.toString() + i.toString()).style.backgroundColor = '#BDBDBD';
             document.getElementById('Trat').style.display = 'none';
             document.getElementById('newT').style.display = 'none';
         }
     }
     fim = false;
     bool = false;
+    colorX = false;
+    colorY = false;
     vez = "nada";
     document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
     document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
     document.getElementById('res').style.display = 'none';
     document.getElementById('newG').style.display = 'block';
     document.getElementById('newGV').style.display = 'none';
+    document.getElementById('EscO').src = 'Bola.png';
+    document.getElementById('EscX').src = 'XIS.png';
 }
-function Recomeco(){
+function Recomeco() {
     NewGame()
-    conto=0;
-    contx=0;
+    conto = 0;
+    contx = 0;
     document.getElementById('PO').innerHTML = conto;
     document.getElementById('PX').innerHTML = contx;
+}
+function Foco(Localizar) {
+    if (colorX == false) {
+        if (Localizar.id == "Xizinho") {
+            document.getElementById('Xizinho').style.backgroundColor = '#212121';
+            document.getElementById('EscX').src = 'XISFoco.png';
+        }
+    }
+    if (colorY == false) {
+        if (Localizar.id == "Bolinha") {
+            document.getElementById('Bolinha').style.backgroundColor = '#212121';
+            document.getElementById('EscO').src = 'BolaFoco.png';
+        }
+    }
+
+
+}
+function NoFoco(Local) {
+    if (colorX == false) {
+        if (Local.id == "Xizinho") {
+            document.getElementById('Xizinho').style.backgroundColor = '#BDBDBD';
+            document.getElementById('EscX').src = 'XIS.png';
+        }
+    } if (colorY == false) {
+        if (Local.id == "Bolinha") {
+            document.getElementById('Bolinha').style.backgroundColor = '#BDBDBD';
+            document.getElementById('EscO').src = 'Bola.png';
+
+        }
+    }
+}
+function MouseUp(Selecao){
+if (colorX == false) {
+    if (Selecao.id == "Xizinho") {
+        document.getElementById('Xizinho').style.top='-1px';
+    }
+}if (colorY == false) {
+    if (Selecao.id == "Bolinha") {
+        document.getElementById('Bolinha').style.top='-1px';
+
+    }
+}
+}
+function MouseDown(Selec){
+    if (colorX == false) {
+        if (Selec.id == "Xizinho") {
+            document.getElementById('Xizinho').style.top='1px';
+        }
+    }if (colorY == false) {
+        if (Selec.id == "Bolinha") {
+            document.getElementById('Bolinha').style.top='1px';
+    
+        }
+    }
 }
